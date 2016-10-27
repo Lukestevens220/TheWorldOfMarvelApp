c61bdbaf0c5569887ff53aa2de94832660b2a5ea
Luke Stevens <Luke.stevens220@hotmail.com>
2016-10-27 17:27:52 +0100

    Initial commit

---
+++ app/src/main/java/appexperts/com/theworldofmarvelapp/MainActivity.java
@@ -0,0 +1,27 @@
+package appexperts.com.theworldofmarvelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+/**
+ * @version 1.1
+ * @author Luke
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "uukA3szldtWU2fIk1ljHsfLqh";
+    private static final String TWITTER_SECRET = "YAPR3DanSeIi4tEFTwgQWDbtD9zdCMwJMSoZbrNXx2WMSI7WGT";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_main);
+    }
+}
